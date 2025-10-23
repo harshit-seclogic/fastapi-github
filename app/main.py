955ee3bd4ff00c0c0000938252311463f7e6607c
harshit <harshit.b@seclogic.io>
2025-10-23 16:45:15 +0530

    fix deployment

--- app/main.py
+++ app/main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello from FastAPI on GKE!", "host": socket.gethostname()}
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
